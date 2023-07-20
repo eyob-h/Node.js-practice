5207ae672366f5e28a4560db9764ad9c0a8e53c2
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-07-20 11:31:02 +0300

    create contact routes

--- Dipesh-Malvia's-node.js-course/mycontacts-backend/server.js
+++ Dipesh-Malvia's-node.js-course/mycontacts-backend/server.js
@@ -7,7 +7,13 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
   });
+
+// app.get("/api/contact",(req,res)=>{
+//     console.log("Welcome");
+//     res.send("Welcome")
+// })
