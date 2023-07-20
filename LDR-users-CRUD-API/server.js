2b5346c9daec1fac6432e73e95ef13261282b308
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-07-20 19:14:15 +0300

    setup database connection

--- LDR-users-CRUD-API/server.js
+++ LDR-users-CRUD-API/server.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+const connectDb = require("./config/dbConnection")
+
+connectDb();
+
+app.use(express.json()); //This won't work if you put it down here
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+app.get("/users",(req,res)=>{
+    console.log("Welcome!");
+    res.send("Welcome!");
+})
+
+// app.get("/api/contact",(req,res)=>{
+//     console.log("Welcome");
+//     res.send("Welcome")
+// })
