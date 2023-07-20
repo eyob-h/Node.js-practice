e45165cd84ab75b0f85e657a2defabd29ae02e82
Eyob <110186516+eyob-h@users.noreply.github.com>
2023-07-20 12:45:13 +0300

    create error handling middleware

--- Dipesh-Malvia's-node.js-course/mycontacts-backend/controllers/contactControllers.js
+++ Dipesh-Malvia's-node.js-course/mycontacts-backend/controllers/contactControllers.js
@@ -1,20 +1,28 @@
+const asyncHandler = require("express-async-handler");
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access private
 
-const getContacts = (req,res)=>{
+const getContacts = asyncHandler( async (req,res)=>{
     res.status(200).json({message:"Get all contacts"})
-};
+});
 
 //2 for create contact.
 //@desc Create contact
 //@route POST /api/contacts
 //@access private
 
-const createContact = (req,res)=>{
+const createContact = asyncHandler( async (req,res)=>{
     console.log("The request body is",req.body)
+
+    const { name, age } = req.body;
+    if (!name || !age ) {
+        res.status(400);
+        throw new Error("All fields are mandatory dude!");
+    }
+
     res.status(201).json({message:"Create contact"})
-}
+});
 
 
 //3 for get a contact.
@@ -22,25 +30,25 @@ const createContact = (req,res)=>{
 //@route GET /api/contacts/:id
 //@access private
 
-const getContact = (req,res)=>{
+const getContact = asyncHandler( async (req,res)=>{
         res.status(200).json({message:`Get contact of ${req.params.id} `})
-    }
+    });
 //for update contact.
 //@desc Get contact
 //@route POST /api/contacts/:id
 //@access private
 
-const updateContact = (req,res)=>{
+const updateContact = asyncHandler( async (req,res)=>{
     res.status(200).json({message:`Update contact of ${req.params.id} `})
-}
+});
 //for update contact.
 //@desc Delete contact
 //@route POST /api/contacts/:id
 //@access private
 
-const deleteContact = (req,res)=>{
+const deleteContact = asyncHandler( async (req,res)=>{
     res.status(200).json({message:`Delete contact of ${req.params.id} `})
-}
+});
 
 
 
--- Dipesh-Malvia's-node.js-course/mycontacts-backend/server.js
+++ Dipesh-Malvia's-node.js-course/mycontacts-backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const connectDb = require("./config/dbConnection");
-// const errorHandler = require("./middleware/errorHandler");
+const errorHandler = require("./middleware/errorHandler")
 const dotenv = require("dotenv").config();
 
 const app = express();
@@ -9,6 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json()); //This won't work if you put it down here
 app.use("/api/contacts", require("./routes/contactRoutes"));
 // app.use(express.json()); //Right here
+app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
